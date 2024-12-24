78510ca8ac6e2aca5e585d38682767809ff3f1fc
Baruch Lavy <baruchlavy@gmail.com>
2024-12-24 13:17:26 +0200

    Edit the onClick function

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function onBallClick(el){
-   el.style.width = 150 + 'px'
-   el.style.height = 150 + 'px'
-   el.innerText = '150'
-}
+function onBallClick(el) {
+    let computedStyle = window.getComputedStyle(el)
+    let currentWidth = parseInt(computedStyle.width)
+    let currentHeight = parseInt(computedStyle.height)
+
+    el.style.width = (currentWidth + 50) + 'px'
+    el.style.height = (currentHeight + 50) + 'px'
+    el.innerText = currentWidth + 50
+
+    if(currentWidth >= 400) {
+        el.style.width = '100px'
+        el.style.height = '100px'
+        el.innerText = 100
+    }
+    
+ }
+ 
+ 
